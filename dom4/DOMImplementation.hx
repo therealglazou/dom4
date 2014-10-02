24035e9dda7976c2dcd59b3506db9e21bb506397
Daniel Glazman <daniel@glazman.org>
2014-10-02 10:25:30 +0200

    XML Name matching, needed for QNames

--- dom4/DOMImplementation.hx
+++ dom4/DOMImplementation.hx
@@ -42,6 +42,10 @@ package dom4;
  */
 class DOMImplementation {
 
+  static private inline var NAME_START_CHAR = "A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD";
+  static private inline var NAME_CHAR = NAME_START_CHAR + "\\-\\.0-9\u00B7\u0300-\u036F\u203F-\u2040";
+  static public  inline var NAME = "^[" + NAME_START_CHAR + "][" + NAME_CHAR + "]*$";
+
   public function new() {
   }
 
